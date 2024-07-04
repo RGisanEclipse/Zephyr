4cee5660ffea6ff86120d02f89eba329d46c99b4
Rishab <rishab28guleria@gmail.com>
2024-07-04 19:15:35 +0530

    UI Improvements

--- Zephyr/Views/Cells/ProfileTabs/ProfileTabsCollectionReusableView.swift
+++ Zephyr/Views/Cells/ProfileTabs/ProfileTabsCollectionReusableView.swift
@@ -6,28 +6,45 @@
 //
 
 import UIKit
-protocol ProfileTabsCollectionReusableViewDelegate: AnyObject{
+
+protocol ProfileTabsCollectionReusableViewDelegate: AnyObject {
     func didTapPostsButton()
     func didTapVideoPostsButton()
     func didTapTaggedUserPostsButton()
 }
+
 class ProfileTabsCollectionReusableView: UICollectionReusableView {
 
     @IBOutlet weak var postsButton: UIButton!
     @IBOutlet weak var videoPostsButton: UIButton!
     @IBOutlet weak var taggedUserPostsButton: UIButton!
     weak var delegate: ProfileTabsCollectionReusableViewDelegate?
+
     override func awakeFromNib() {
         super.awakeFromNib()
     }
+
+    override func layoutSubviews() {
+       super.layoutSubviews()
+       setupBottomBorder()
+    }
+    
     @IBAction func postsButtonTapped(_ sender: UIButton) {
         delegate?.didTapPostsButton()
     }
+
     @IBAction func videoPostsButtonTapped(_ sender: UIButton) {
         delegate?.didTapVideoPostsButton()
     }
-    
+
     @IBAction func taggedUserPostsButtonTapped(_ sender: UIButton) {
         delegate?.didTapTaggedUserPostsButton()
+    }
+
+    private func setupBottomBorder() {
+        let bottomBorder = CALayer()
+        bottomBorder.frame = CGRect(x: 0.0, y: self.frame.height - 1.0, width: self.frame.width, height: 1.0)
+        bottomBorder.backgroundColor = UIColor.gray.cgColor
+        self.layer.addSublayer(bottomBorder)
     }
 }
