e74bb834d96466f7d565c4fdddd4b34edf81b0f4
Rishab <rishab28guleria@gmail.com>
2024-07-11 16:43:51 +0530

    Added option to let users comment

--- Zephyr/Controllers/Others/General/CommentsViewController.swift
+++ Zephyr/Controllers/Others/General/CommentsViewController.swift
@@ -8,8 +8,13 @@
 import UIKit
 
 class CommentsViewController: UIViewController {
+    
     var model: UserPost?
+    private var userData = UserModel(userName: "TheBatman", profilePicture: URL(string: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3yWDu-i3sbrtGUoAnYqKyZcf-RbSRqsRtYg&s")!, bio: "It's not who you are underneath, it's what you do, that defines you.", name: (first: "Bruce", last: "Wayne"), birthDate: Date(), gender: .male, counts: UserCount(posts: 1, followers: 0, following: 0), joinDate: Date(), followers: [], following: [])
+    
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var userProfileImage: UIImageView!
+    @IBOutlet weak var commentsTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.isHidden = false
@@ -17,8 +22,19 @@ class CommentsViewController: UIViewController {
         tableView.register(UINib(nibName: Constants.Post.generalCellIdentifier, bundle: nil), forCellReuseIdentifier: Constants.Post.generalCellIdentifier)
         tableView.register(UINib(nibName: Constants.Post.commentsHeaderCellIdentifier, bundle: nil), forCellReuseIdentifier: Constants.Post.commentsHeaderCellIdentifier)
         tableView.dataSource = self
+        userProfileImage.sd_setImage(with: userData.profilePicture, placeholderImage: UIImage(systemName: "person.circle.fill"))
+        userProfileImage.layer.cornerRadius = userProfileImage.frame.size.width / 2
+        userProfileImage.layer.masksToBounds = true
+        if let safeModel = model{
+            commentsTextField.placeholder = "Add a comment to \(safeModel.owner.userName)'s post"
+        }
+        commentsTextField.delegate = self
     }
-    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.navigationBar.isHidden = false
+    }
+    @IBAction func postCommentButtonPressed(_ sender: UIButton) {
+    }
 }
 
 // MARK: - UITableViewDataSource
@@ -56,5 +72,18 @@ extension CommentsViewController: UITableViewDataSource{
 extension CommentsViewController: CommentsHeaderTableViewCellDelegate{
     func didTapPostButton() {
         navigationController?.popViewController(animated: true)
+    }
+}
+
+// MARK: - UITextFieldDelegate
+extension CommentsViewController: UITextFieldDelegate{
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        self.navigationController?.navigationBar.isHidden = true
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        self.navigationController?.navigationBar.isHidden = false
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
     }
 }
